17e7560140560e002e9ffc96258c988a7410c39a
Rupeshknn <rupeshknn@gmail.com>
2020-08-17 14:34:18 +0530

    empty textfiles added to each folder

--- dir_creator.py
+++ dir_creator.py
@@ -1,9 +1,13 @@
 import os
 subs =['Physics', 'Chemistry', 'Systems_Biology', 'Mathematics', 'Earth_Science']
+
 for j in subs:
     str1='mkdir {}'.format(j)
     os.system(str1)
     for i in range(1,11):
         str3='cd {} && mkdir Sem_{}'.format(j,i)
         os.system(str3)
-        
+for j in subs:
+    for i in range(1,11):
+        str3='cd {}/Sem_{} && echo empty > tt.txt'.format(j,i)
+        os.system(str3)        
